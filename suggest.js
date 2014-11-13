5ffed3debe2a836a87dd8713bcb56dbd1fd32c38
Cristiano Santos <cristianossd@gmail.com>
2014-11-13 14:40:55 -0300

    Getting parameters of inputs #1

--- suggest.js
+++ suggest.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
 	$("form").submit(function() {
 		event.preventDefault();
+		year = $("form input[name='year']").val();
+		actors = $("form input[name='actors']").val();
+		actor = actors.split(",");
+		toReplace = /\s/g;
+		for (i=0; i<actor.length; i++) {
+			actor[i] = actor[i].replace(toReplace, "");
+			alert(actor[i]);
+		}
+
+		otherInformation = $("form input[name='other-information']").val();
+		category = $("form select[name='category'] option:selected").val();
+
 		$.ajax("test.html")
 		  .done(function(data) {
-		    alert(data);
 		  })
 			.fail(function(jqXHR, textStatus, errorThrown) {
 			  alert(textStatus);
