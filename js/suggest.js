bc503ecac76b65eb3b2559f6355698852e72439a
Gabriel.Erbetta <gabrielerbetta@gmail.com>
2014-11-13 17:34:21 -0300

    front-end v0.5
    consertado em firefox
    ainda com exemplo

--- js/suggest.js
+++ js/suggest.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-  $(".toggle-section").click(function(){
-    $("#user-request").slideToggle("fast");
-    $("#user-request .row").slideToggle("fast");
-    $("#suggestion").slideToggle("fast");
-  });
+  	$(".toggle-section").click(function(){
+    	$("#user-request").slideToggle("fast");
+    	$("#user-request .row").slideToggle("fast");
+    	$("#suggestion").slideToggle("fast");
+  	});
 });
