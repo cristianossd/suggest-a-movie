ba74ce71b73d964592c2129163cdd17106823aaf
Cristiano Santos <cristianossd@gmail.com>
2014-11-13 21:35:38 -0300

    Generating a random movie without parameters #1

--- suggest.js
+++ suggest.js
@@ -15,7 +15,37 @@ $(document).ready(function(){
 
 		$.ajax("test.html")
 		  .done(function(data) {
-		  	code = $.parseHTML(data);
+		  	$(".hidden").append($(data));
+		  	//custom = $("#3 h2");
+		  	//alert($(".hidden").find(custom).text());
+		  	/*if ($(".hidden").find("#2").text() == "")
+		  		alert("empty");
+		  	*/
+		  	var matched, resultYear, resultActors, resultOtherInformation, resultCategory,
+		  	outImage, outTitle, outYear, outSinpsis, outCategories, outActors, outTrailer;
+
+		  	if (year == "" && actors == "" && otherInformation == "" && category == "") {
+		  		do{
+		  			match = Math.floor(Math.random()*2+3);
+		  			id = "#" + match;
+		  		} while ($(".hidden").find(id).text() == "");
+
+		  		custom = id + " img";
+		  		outImage = $(".hidden").find(custom).attr("src");
+		  		custom = id + " h2";
+		  		outTitle = $(".hidden").find(custom).text();
+		  		custom = id + " span";
+		  		outYear = $(".hidden").find(custom).text();
+		  		custom = id + " .sinopsis";
+		  		outSinopsis = $(".hidden").find(custom).text();
+		  		custom = id + " .categories";
+		  		outCategories = $(".hidden").find(custom).text();
+		  		custom = id + " .actors";
+		  		outActors = $(".hidden").find(custom).text();
+		  	}
+		  	else {
+		  		
+		  	}
 		  })
 			.fail(function(jqXHR, textStatus, errorThrown) {
 			  alert(textStatus);
