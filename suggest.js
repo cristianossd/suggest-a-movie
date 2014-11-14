d1ad10b838df0f1fa312c8a465a1a9ca88110a7a
Cristiano Santos <cristianossd@gmail.com>
2014-11-13 22:33:02 -0300

    Suggesting movies #1

--- suggest.js
+++ suggest.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 		  	/*if ($(".hidden").find("#2").text() == "")
 		  		alert("empty");
 		  	*/
-		  	var matched, resultYear, resultActors, resultOtherInformation, resultCategory,
+		  	var matched = new Array(), resultTitle, resultYear, resultActors, resultOtherInformation, resultCategory,
 		  	outImage, outTitle, outYear, outSinpsis, outCategories, outActors, outTrailer;
 
 		  	if (year == "" && actors == "" && otherInformation == "" && category == "") {
@@ -44,7 +44,82 @@ $(document).ready(function(){
 		  		outActors = $(".hidden").find(custom).text();
 		  	}
 		  	else {
-		  		
+		  		for (i=3; i<4; i++) {
+		  			var count = 0;
+
+		  			id = "#" + i;
+		  			if ($(".hidden").find(id).text() != "") {
+		  				
+		  				if (otherInformation != "") {
+		  					custom = id + " h2";
+			  				if ($(".hidden").find(custom).text().indexOf(otherInformation) >	 -1)
+			  					resultTitle = true;
+			  				else
+			  					resultTitle = false;
+		  				}
+
+	  					if (year != "") {
+		  					custom = id + " span";
+			  				if ($(".hidden").find(custom).text().indexOf(year) > -1)
+			  					resultYear = true;
+			  				else
+			  					resultYear = false;
+			  			}
+
+	  					if (otherInformation != "") {
+		  					custom = id + " .sinopsis";
+			  				if ($(".hidden").find(custom).text().indexOf(otherInformation) > -1)
+			  					resultOtherInformation = true;	
+			  				else
+			  					resultOtherInformation = false;
+			  			}
+
+	  					if (category != "") {
+		  					custom = id + " .categories";
+			  				if ($(".hidden").find(custom).text().indexOf(category) > -1)
+			  					resultCategory = true;
+			  				else
+			  					resultCategory = false;
+			  			}
+
+	  					if (actors != "") {
+		  					custom = id + " .actors";
+			  				for (j=0; j<actor.length; j++)
+			  					if ($(".hidden").find(custom).text().indexOf(actor[i]) > -1)
+			  						count++;
+			  				if (count == actor.length)
+			  					resultActors = true;
+			  				else
+			  					resultActors = false;
+			  			}
+
+			  			if (((otherInformation == "" || resultTitle == true) || (otherInformation == "" || resultOtherInformation == true)) && (year == "" || resultYear == true) && (category == "" || resultCategory == true) && (actors == "" || resultActors == true)) {
+			  				matched[matched.length] = id;
+			  			}
+		  			}
+		  		}
+
+		  		if (matched.length > 0) {
+		  			match = Math.floor(Math.random()*matched.length);
+		  			alert(match);
+		  			suggestionID = matched[match];
+			  		custom = suggestionID + " img";
+			  		outImage = $(".hidden").find(custom).attr("src");
+			  		custom = suggestionID + " h2";
+			  		outTitle = $(".hidden").find(custom).text();
+			  		alert(outTitle);
+			  		custom = suggestionID + " span";
+			  		outYear = $(".hidden").find(custom).text();
+			  		custom = suggestionID + " .sinopsis";
+			  		outSinopsis = $(".hidden").find(custom).text();
+			  		custom = suggestionID + " .categories";
+			  		outCategories = $(".hidden").find(custom).text();
+			  		custom = suggestionID + " .actors";
+			  		outActors = $(".hidden").find(custom).text();
+		  		}
+		  		else {
+		  			// no movies matched
+		  		}
 		  	}
 		  })
 			.fail(function(jqXHR, textStatus, errorThrown) {
