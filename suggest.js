0c663c85c569cd90b278fc6c1325667b7eca81ab
Cristiano Santos <cristianossd@gmail.com>
2014-11-14 00:32:25 -0300

    Finding movies based on parameters and closed #1

--- suggest.js
+++ suggest.js
@@ -12,16 +12,16 @@ $(document).ready(function(){
 		otherInformation = $("form input[name='other-information']").val();
 		category = $("form select[name='category'] option:selected").val();
 
-		$.ajax("movies.html")
+		$.ajax("test.html")
 		  .done(function(data) {
-		  	$(".hidden").append($(data));
+				$(".hidden").append($(data));
 		  	/*custom = $("#3 h2");
 		  	alert($(".hidden").find(custom).text());
 		  	if ($(".hidden").find("#2").text() == "")
 		  		alert("empty");
 		  	*/
 		  	var matched = new Array(), resultTitle, resultYear, resultActors, resultOtherInformation, resultCategory,
-		  	outImage, outTitle, outYear, outSinpsis, outCategories, outActors, outTrailer;
+		  	outImage, outTitle, outYear, outSinopsis, outCategories, outActors, outTrailer;
 
 		  	if (year == "" && actors == "" && otherInformation == "" && category == "") {
 		  		do{
@@ -29,18 +29,38 @@ $(document).ready(function(){
 		  			id = "#" + match;
 		  		} while ($(".hidden").find(id).text() == "");
 
-		  		custom = id + " img";
-		  		outImage = $(".hidden").find(custom).attr("src");
-		  		custom = id + " h2";
-		  		outTitle = $(".hidden").find(custom).text();
-		  		custom = id + " span";
-		  		outYear = $(".hidden").find(custom).text();
-		  		custom = id + " .sinopsis";
-		  		outSinopsis = $(".hidden").find(custom).text();
-		  		custom = id + " .categories";
-		  		outCategories = $(".hidden").find(custom).text();
-		  		custom = id + " .actors";
-		  		outActors = $(".hidden").find(custom).text();
+			  		custom = id + " img";
+			  		outImage = $(".hidden").find(custom).attr("src");
+			  		$("#movie_image img").attr("src", outImage);
+
+			  		custom = id + " h2";
+			  		outTitle = $(".hidden").find(custom).html();
+			  		$("#movie_title").empty();
+			  		$("#movie_title").append(outTitle);
+
+			  		custom = id + " span";
+			  		outYear = $(".hidden").find(custom).html();
+			  		$("#movie_year").empty();
+			  		$("#movie_year").append("Ano: <br />");
+			  		$("#movie_year").append(outYear);
+
+			  		custom = id + " .sinopsis";
+			  		outSinopsis = $(".hidden").find(custom).html();
+			  		$("#movie_sinopsis").empty();
+			  		$("#movie_sinopsis").append("Sinopse: <br />");
+			  		$("#movie_sinopsis").append(outSinopsis);
+
+			  		custom = id + " .categories";
+			  		outCategories = $(".hidden").find(custom).html();
+			  		$("#movie_category").empty();
+			  		$("#movie_category").append("Categoria: <br />");
+			  		$("#movie_category").append(outCategories);
+
+			  		custom = id + " .actors";
+			  		outActors = $(".hidden").find(custom).html();
+			  		$(".movie_actors").empty();
+			  		$(".movie_actors").append("Atores: ");
+			  		$(".movie_actors").append(outActors);
 
 		  		// call youtube API
 		  	}
@@ -103,18 +123,39 @@ $(document).ready(function(){
 		  		if (matched.length > 0) {
 		  			match = Math.floor(Math.random()*matched.length);
 		  			suggestionID = matched[match];
+
 			  		custom = suggestionID + " img";
 			  		outImage = $(".hidden").find(custom).attr("src");
+			  		$("#movie_image img").attr("src", outImage);
+
 			  		custom = suggestionID + " h2";
-			  		outTitle = $(".hidden").find(custom).text();
+			  		outTitle = $(".hidden").find(custom).html();
+			  		$("#movie_title").empty();
+			  		$("#movie_title").append(outTitle);
+
 			  		custom = suggestionID + " span";
-			  		outYear = $(".hidden").find(custom).text();
+			  		outYear = $(".hidden").find(custom).html();
+			  		$("#movie_year").empty();
+			  		$("#movie_year").append("Ano: <br />");
+			  		$("#movie_year").append(outYear);
+
 			  		custom = suggestionID + " .sinopsis";
-			  		outSinopsis = $(".hidden").find(custom).text();
+			  		outSinopsis = $(".hidden").find(custom).html();
+			  		$("#movie_sinopsis").empty();
+			  		$("#movie_sinopsis").append("Sinopse: <br />");
+			  		$("#movie_sinopsis").append(outSinopsis);
+
 			  		custom = suggestionID + " .categories";
-			  		outCategories = $(".hidden").find(custom).text();
+			  		outCategories = $(".hidden").find(custom).html();
+			  		$("#movie_category").empty();
+			  		$("#movie_category").append("Categoria: <br />");
+			  		$("#movie_category").append(outCategories);
+
 			  		custom = suggestionID + " .actors";
-			  		outActors = $(".hidden").find(custom).text();
+			  		outActors = $(".hidden").find(custom).html();
+			  		$(".movie_actors").empty();
+			  		$(".movie_actors").append("Atores: ");
+			  		$(".movie_actors").append(outActors);
 
 			  		// call youtube API
 		  		}
